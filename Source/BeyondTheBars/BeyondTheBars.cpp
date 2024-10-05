59d78949f7b96c1fdd56f940688b90deaad7a0d7
Anderson Lima <andersonaraujodl@gmail.com>
2024-10-05 19:37:04 -0400

    first commit - project creation

---
+++ Source/BeyondTheBars/BeyondTheBars.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BeyondTheBars.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BeyondTheBars, "BeyondTheBars" );
+ 
---
+++ Source/BeyondTheBars/BeyondTheBarsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BeyondTheBarsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABeyondTheBarsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABeyondTheBarsGameMode();
+};
+
+
+
